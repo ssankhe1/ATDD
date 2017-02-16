ac00cd226367e7294da6dccedf434e533cf9daf7
Sankhe <ssankhe1@ms.ds.uhc.com>
2017-02-16 15:21:05 -0600

    newgitrunnerclass

--- src/test/java/TestRunner/RunnerTest.java
+++ src/test/java/TestRunner/RunnerTest.java
@@ -9,11 +9,11 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/Resources",glue={"C3.C3Testing"}, tags= {"@smoke"},
 format={"html:target/cucumber-html-report"},
-plugin = {"pretty", "html:target/cucumber-html-report"})
+plugin = {"pretty", "html:target/cucumber-html-report", "json:target/cucumber-html-report/cucumber.json"})
 
 public class RunnerTest {
 
-	//, "json:target/cucumber-html-report/cucumber.json"
+	//
 	
 
 }
